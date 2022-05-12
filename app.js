1f216ff4bc50320b7f82d13887f204fa44fef525
Sunny Chung <78622789+yatyichung@users.noreply.github.com>
2022-05-11 23:57:27 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+//customize yargs version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder:{
+        title:{
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
+
+//creat remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    builder: {
+        title:{
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title)
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list a note',
+    handler(){
+        notes.listNotes()
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'reading a note',
+    builder:{
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
+
